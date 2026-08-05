958d623113477afb8f6023b6b206ecc0c37ec648
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-08-05 15:41:11 -0400

    feat: moviendo de posición los botones de agregar eliminar y editar tareas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import { TaskModal } from './components/TaskModal'
-import { useTasks } from './hooks/useTasks'
+import { useState } from 'react';
+import { TaskModal } from './components/TaskModal';
+import { useTasks } from './hooks/useTasks';
+import { Plus, Trash, Pencil } from 'lucide-react';
 
 function App() {
   const [showModal, setShowModal] = useState(false)
@@ -13,32 +14,45 @@ function App() {
           <p>Este será el header</p>
         </header>
         <main className='p-4'>
-          <h2 className='text-2xl font-bold m-2'>Lista de Tareas</h2>
+          <div className='flex items-center justify-between'>
+            <h2 className='text-2xl font-bold m-2'>Lista de Tareas</h2>
+            <button 
+              className='bg-blue-300 text-black p-0.5 rounded m-2 hover:bg-blue-600 hover:cursor-pointer hover:text-white' 
+              onClick={() => setShowModal(true)} 
+              type='button'
+            > 
+              <Plus className='w-4 h-4' />
+            </button>
+            <TaskModal isOpen={showModal} onClose={() => setShowModal(false)} onAddTask={handleAddTask} />
+          </div>
           {loading && <p>Cargando...</p>}
           {error && <p>{error}</p>}
           <ul className=''>
             {Array.isArray(tasks) ? (
               tasks.map((task) => (
-                <li key={task.id} className='border border-gray-300 rounded p-2 m-2'>
-                  <h3 className='text-xl font-bold'>{task.title}</h3>
-                  <p className='text-gray-600'>{task.description}</p>
-                  <p className='text-gray-600'>Completada: {task.completed ? 'Sí' : 'No'}</p>
+                <li key={task.id} className='border border-gray-300 rounded p-2 m-2 '>
+                  <div className='flex items-center justify-between'>
+                    <h3 className='text-xl font-bold'>{task.title}</h3>
+                    <div>
+                      <button className='bg-red-300 text-black p-0.5 rounded m-2 hover:bg-red-600 hover:cursor-pointer hover:text-white'>
+                        <Trash className='w-4 h-4' />
+                      </button>
+                      <button className='bg-green-300 text-black p-0.5 rounded m-2 hover:bg-green-600 hover:cursor-pointer hover:text-white'>
+                        <Pencil className='w-4 h-4' />
+                      </button>
+                    </div>
+                  </div>
+                  <div>
+                    <p className='text-gray-600'>{task.description}</p>
+                    <p className='text-gray-600'>Completada: {task.completed ? 'Sí' : 'No'}</p>
+                  </div>
                 </li>
               ))
             ) : (
               !loading && !error && <p>No se pudieron cargar las tareas correctamente.</p>
             )}
           </ul>
-          <button 
-            className='bg-blue-500 text-white p-2 rounded m-2 hover:bg-blue-600 hover:cursor-pointer' 
-            onClick={() => setShowModal(true)} 
-            type='button'
-          > 
-            Agregar Tarea 
-          </button>
-          <TaskModal isOpen={showModal} onClose={() => setShowModal(false)} onAddTask={handleAddTask} />
-          <button className='bg-red-500 text-white p-2 rounded m-2 hover:bg-red-600 hover:cursor-pointer'>Eliminar Tarea</button>
-          <button className='bg-green-500 text-white p-2 rounded m-2 hover:bg-green-600 hover:cursor-pointer'>Editar Tarea</button>
+
         </main>
         <footer>
           <p>Este será el footer</p>
